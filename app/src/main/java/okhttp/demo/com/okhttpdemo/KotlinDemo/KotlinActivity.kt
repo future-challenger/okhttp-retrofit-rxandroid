07aa2847746ccc3a6307696f6559b0c2c8146355
future-challenger <sunshine_anycall@hotmail.com>
2016-02-25 18:30:16 +0800

    kotlin demo activity/Users/uncle_charlie/IdeaProjects/OkHttpDemo/app/src/main/java/okhttp/demo/com/okhttpdemo/KotlinDemo/KotlinActivity.kt

---
+++ app/src/main/java/okhttp/demo/com/okhttpdemo/KotlinDemo/KotlinActivity.kt
@@ -0,0 +1,20 @@
+package okhttp.demo.com.okhttpdemo.KotlinDemo
+
+import android.app.Activity
+import android.os.Bundle
+import okhttp.demo.com.okhttpdemo.R
+
+/**
+ * Created by uncle_charlie on 25/2/16.
+ */
+class KotlinActivity : Activity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+
+        var textview = findViewById(R.id.kotlin_textview)
+//        textview
+    }
+}
